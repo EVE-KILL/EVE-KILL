af883e751781317cf3701276e6c6c3a6081a382b
Michael Karbowiak <michael@karbowiak.dk>
2023-10-18 12:53:24 +0200

    Refactoring and cleaning up

--- app/Commands/Fetch/FetchKillmails.php
+++ app/Commands/Fetch/FetchKillmails.php
@@ -14,55 +14,54 @@ class FetchKillmails extends ConsoleCommand
 
     public function __construct(
         protected ClassLoader $autoloader,
-        protected killmails $killmails,
+        protected Killmails $killmails,
     ) {
         parent::__construct();
     }
 
+    protected function fetchAndCacheData($date): array
+    {
+        $file = \BASE_DIR . "/resources/cache/{$date}.json";
+
+        if (file_exists($file)) {
+            return json_decode(file_get_contents($file), true);
+        } else {
+            $this->out("Fetching from: https://zkillboard.com/api/history/{$date}.json");
+            $kills = file_get_contents("https://zkillboard.com/api/history/{$date}.json");
+
+            if (!empty($kills)) {
+                file_put_contents($file, $kills);
+                return json_decode($kills, true);
+            } else {
+                throw new \RuntimeException('Kills was empty');
+            }
+        }
+    }
 
     final public function handle(): void
     {
-        // Killmails processed
         $processed = 1;
-        // Killmails in total
         $totalKillmails = 0;
 
-        // Get the totals available from zKillboard
         $totalData = file_get_contents('https://zkillboard.com/api/history/totals.json') ?? [];
         $totalAvailable = new Collection(json_decode($totalData, true, flags: \JSON_THROW_ON_ERROR));
 
-        (new Collection($totalAvailable))->transform(static function ($row) use (&$totalKillmails) {
+        $totalAvailable->each(function ($row) use (&$totalKillmails) {
             $totalKillmails += $row;
         });
 
         $this->out('Iterating over ' . count($totalAvailable) . ' individual days');
+
         foreach ($totalAvailable->reverse() as $date => $total) {
             $this->out("Day: {$date} | {$total} kills available");
+
             try {
-                $file = \BASE_DIR . "/resources/cache/{$date}.json";
-                if (!file_exists($file)) {
-                    $this->out("Fetching from: https://zkillboard.com/api/history/{$date}.json");
-                    $kills = file_get_contents("https://zkillboard.com/api/history/{$date}.json");
-                    if (!empty($kills)) {
-                        file_put_contents($file, $kills);
-                        $kills = json_decode($kills, true);
-                    } else {
-                        throw new \RuntimeException('Kills was empty');
-                    }
-                } else {
-                    $kills = json_decode(
-                        file_get_contents($file),
-                        true
-                    );
-                }
+                $kills = $this->fetchAndCacheData($date);
             } catch (\Exception $e) {
                 sleep(10);
-                $this->out("Fetching from: https://zkillboard.com/api/history/{$date}.json");
-                $kills = json_decode(
-                    file_get_contents("https://zkillboard.com/api/history/{$date}.json"),
-                    true
-                );
+                $kills = $this->fetchAndCacheData($date);
             }
+
             foreach ($kills as $killId => $hash) {
                 if ($killId === 'day') {
                     continue;
@@ -80,6 +79,7 @@ class FetchKillmails extends ConsoleCommand
                     'hash' => $hash,
                     'fetched' => false,
                 ]);
+
                 try {
                     $this->killmails->save();
                 } catch (\Exception $e) {
